773691e24e58bcf56f9796ee47db91b17c4e5fc7
Андрій <Andrii Hromov>
2020-02-05 16:29:23 +0200

    Removed from web.xml

--- src/main/java/ua/lviv/lgs/servlet/RegistrationServlet.java
+++ src/main/java/ua/lviv/lgs/servlet/RegistrationServlet.java
@@ -3,6 +3,7 @@ package ua.lviv.lgs.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import ua.lviv.lgs.domain.User;
 import ua.lviv.lgs.domain.UserRole;
 import ua.lviv.lgs.service.impl.UserServiceImpl;
 
+@WebServlet("/registration")
 public class RegistrationServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -9186251900623717347L;
