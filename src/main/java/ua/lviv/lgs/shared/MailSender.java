861bef9a73d300bb200d044506e9611f37b0c30b
Андрій <Andrii Hromov>
2020-02-11 01:19:27 +0200

    Now sendMail() will thows exceptions

--- src/main/java/ua/lviv/lgs/shared/MailSender.java
+++ src/main/java/ua/lviv/lgs/shared/MailSender.java
@@ -9,6 +9,7 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -19,7 +20,7 @@ import ua.lviv.lgs.dao.impl.BucketDaoImpl;
 
 public class MailSender {
 
-	private final static Logger log = LogManager.getLogger(BucketDaoImpl.class.getName());
+	private final static Logger log = LogManager.getLogger(MailSender.class.getName());
 	private final static Properties prop = new Properties();
 
 	private static MailSender ms;
@@ -40,24 +41,20 @@ public class MailSender {
 		return ms;
 	}
 
-	public void sendMail(String to, String subject, String text) {
+	public void sendMail(String to, String subject, String text) throws AddressException, MessagingException {
 		Session session = Session.getInstance(prop, new Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(prop.getProperty("username"), prop.getProperty("password"));
 			}
 		});
 
-		try {
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(prop.getProperty("from")));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-			message.setSubject(subject);
-			message.setText(text);
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(prop.getProperty("from")));
+		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+		message.setSubject(subject);
+		message.setText(text);
 
-			Transport.send(message);
-		} catch (MessagingException e) {
-			log.error("Can`t send mail: ", e);
-		}
+		Transport.send(message);
 	}
 
 }
